aadbb6b2ed7464a6400e9db993b3fbdaf39555b3
Virginie LE GUEN BERTHEAUME <virginie.leguenbertheaume@hellocway.com>
2016-04-07 15:25:11 +0200

    v2.1 : volume

--- js/recordFn.js
+++ js/recordFn.js
@@ -1,7 +1,6 @@
 (function(window){
 
 	var WORKER_PATH = 'js/recorderWorker.js';
-	var encoderWorker = new Worker('js/mp3Worker.js');
 	var elem;
 	var Recorder = function(source, cfg){
 		var config = cfg || {};
@@ -21,6 +20,7 @@
 		});
 		var recording = false,
 			currCallback;
+		var hasStopped = 0;
 
 		this.node.onaudioprocess = function(e){
 			if (!recording) return;
@@ -45,10 +45,12 @@
 		this.record = function(button){
 			elem = button;
 			recording = true;
+			hasStopped = 0;
 		}
 
 		this.stop = function(){
 			recording = false;
+			hasStopped++;
 		}
 
 		this.clear = function(){
@@ -61,78 +63,20 @@
 		}
 
 		this.exportWAV = function(cb, type){
-			currCallback = cb || config.callback;
-			type = type || config.type || 'audio/wav';
-			if (!currCallback) throw new Error('Callback not set');
-			worker.postMessage({
-				command: 'exportWAV',
-				type: type
-			});
+			if( hasStopped <= 1 ) {
+				currCallback = cb || config.callback;
+				type = type || config.type || 'audio/wav';
+				if (!currCallback) throw new Error('Callback not set');
+				worker.postMessage({
+					command: 'exportWAV',
+					type: type
+				});
+			}
 		}
 
-	//Mp3 conversion
 		worker.onmessage = function(e){
 			
 			var blob = e.data;
-		// console.log("the blob ", blob , blob.size + " " + blob.type);
-		/*
-		var arrayBuffer;
-		var fileReader = new FileReader();
-
-		fileReader.onload = function(){
-		arrayBuffer = this.result;
-		var buffer = new Uint8Array(arrayBuffer),
-				data = parseWav(buffer);
-
-				console.log(data);
-		console.log("Converting to Mp3");
-		log.innerHTML += "\n" + "Converting to Mp3";
-
-				encoderWorker.postMessage({ cmd: 'init', config:{
-						mode : 3,
-			channels:1,
-			samplerate: data.sampleRate,
-			bitrate: data.bitsPerSample
-				}});
-
-				encoderWorker.postMessage({ cmd: 'encode', buf: Uint8ArrayToFloat32Array(data.samples) });
-				encoderWorker.postMessage({ cmd: 'finish'});
-				encoderWorker.onmessage = function(e) {
-						if (e.data.cmd == 'data') {
-
-				console.log("Done converting to Mp3");
-				log.innerHTML += "\n" + "Done converting to Mp3";
-
-				// var audio = new Audio();
-				// audio.src = 'data:audio/mp3;base64,'+encode64(e.data.buf);
-				// audio.play();
-
-				//console.log ("The Mp3 data " + e.data.buf);
-
-				var mp3Blob = new Blob([new Uint8Array(e.data.buf)], {type: 'audio/mp3'});
-				uploadAudio(mp3Blob);
-
-				var url = 'data:audio/mp3;base64,'+encode64(e.data.buf);
-				var li = document.createElement('li');
-				var au = document.createElement('audio');
-				var hf = document.createElement('a');
-
-				au.controls = true;
-				au.src = url;
-				hf.href = url;
-				hf.download = 'audio_recording_' + new Date().getTime() + '.mp3';
-				hf.innerHTML = hf.download;
-				li.appendChild(au);
-				li.appendChild(hf);
-				recordingslist.appendChild(li);
-
-						}
-				};
-		};
-
-		fileReader.readAsArrayBuffer(blob);
-
-			*/
 			currCallback(blob , elem );
 		}
 
@@ -171,6 +115,7 @@
 	}
 
 	function Uint8ArrayToFloat32Array(u8a){
+		console.log("called");
 		var f32Buffer = new Float32Array(u8a.length);
 		for (var i = 0; i < u8a.length; i++) {
 			var value = u8a[i<<1] + (u8a[(i<<1)+1]<<8);
@@ -180,42 +125,9 @@
 		return f32Buffer;
 	}
 
-	function uploadAudio(mp3Data){
-		var reader = new FileReader();
-		reader.onload = function(event){
-			var fd = new FormData();
-			var mp3Name = encodeURIComponent('audio_recording_' + new Date().getTime() + '.mp3');
-			console.log("mp3name = " + mp3Name);
-			fd.append('fname', mp3Name);
-			fd.append('data', event.target.result);
-			$.ajax({
-				type: 'POST',
-				url: 'upload.php',
-				data: fd,
-				processData: false,
-				contentType: false
-			}).done(function(data) {
-				//console.log(data);
-				log.innerHTML += "\n" + data;
-			});
-		};
-		reader.readAsDataURL(mp3Data);
-	}
-
 		source.connect(this.node);
-		this.node.connect(this.context.destination);    //this should not be necessary
+		this.node.connect(this.context.destination); //this should not be necessary
 	};
-
-	/*Recorder.forceDownload = function(blob, filename){
-	console.log("Force download");
-		var url = (window.URL || window.webkitURL).createObjectURL(blob);
-		var link = window.document.createElement('a');
-		link.href = url;
-		link.download = filename || 'output.wav';
-		var click = document.createEvent("Event");
-		click.initEvent("click", true, true);
-		link.dispatchEvent(click);
-	}*/
 
 	window.Recorder = Recorder;
 
--- js/recordMain.js
+++ js/recordMain.js
@@ -1,23 +1,15 @@
-function __log(e, data) {
-	// log.innerHTML += "\n" + e + " " + (data || '');
-}
-
 var audio_context;
 var recorder;
 
 function startUserMedia(stream) {
 	var input = audio_context.createMediaStreamSource(stream);
-	__log('Media stream created.' );
-	__log("input sample rate " +input.context.sampleRate);
 
 	// Feedback!
 	//input.connect(audio_context.destination);
-	__log('Input connected to audio context destination.');
 
 	recorder = new Recorder(input, {
 								numChannels: 1
 							});
-	__log('Recorder initialised.');
 }
 
 function startRecording(button) {
@@ -26,20 +18,21 @@ function startRecording(button) {
 	$( ".record" ).attr( "disabled" , true );
 	$( button.nextElementSibling ).removeClass( "hidden" );
 
-  var value = parseInt( $( "#timeRecording" ).val( ) );
-  if( value < 0 || !value )
-      value = 4;
+	var value = parseInt( $( "#timeRecording" ).val( ) );
+	if( value < 0 || !value )
+		value = 4;
 
-  setTimeout( function( ) {
-    $( button.nextElementSibling ).trigger("click");//.bind( $( button.nextElementSibling ) )
-  } , value*1000 );
+	setTimeout( function( ) {
+		$( button.nextElementSibling ).trigger("click");
+	} , value*1000 );
 
-	__log('Recording...');
 	var p = $( button ).parent( );
 	var pl = p.find( ".play" );
 	var loop = p.find( ".loop" );
+	var slider = p.find( ".slider" );
 	pl.addClass( "hidden" )
 	loop.addClass( "hidden" )
+	slider.addClass( "hidden" )
 }
 
 function stopRecording(button) {
@@ -47,7 +40,6 @@ function stopRecording(button) {
 	$( button ).addClass( "hidden" );
 	$( ".record" ).attr( "disabled" , false );
 	$( button.previousElementSibling ).removeClass( "hidden" );
-	__log('Stopped recording.');
 
 	// create WAV download link using audio data blob
 	createDownloadLink();
@@ -56,50 +48,20 @@ function stopRecording(button) {
 }
 
 function createDownloadLink() {
-	recorder && recorder.exportWAV(function(blob,elem) {
+	recorder & recorder.exportWAV(function(blob,elem) {
 		var url = URL.createObjectURL(blob);
 		var p = $( elem ).parent( );
 		var au = p.find( "audio" );
 		var pl = p.find( ".play" );
 		var loop = p.find( ".loop" );
+		var slider = p.find( ".slider" );
 		au.attr( "src" , url );
 		pl.removeClass( "hidden" );
 		loop.removeClass( "hidden" );
-		clickPlay( pl );
-		clickLoop( loop );
+		slider.removeClass( "hidden" );
 	} );
 }
 
-function clickPlay( play ) {
-	play.click( function( ) {
-
-		var audio = play.parent( ).find( "audio" )[ 0 ];
-		audio.onended = function() {
-		    // console.log('music stopped');
-			play.text( "Play" );
-		}
-		if (audio.paused == false || play.text( ) === "Stop" ) {
-		      audio.pause();
-		      audio.currentTime = 0;
-		      // console.log('music paused');
-			  play.text( "Play" );
-		  } else {
-		      audio.play();
-		      audio.currentTime = 0;
-		      // console.log('music playing');
-			  play.text( "Stop" );
-		  }
-	} );
-}
-function clickLoop( loop ) {
-
-	loop.click( function( ) {
-		var audio = loop.parent( ).find( "audio" )[ 0 ];
-		loop.toggleClass( "btn-default" );
-		loop.toggleClass( "btn-info" );
-		audio.loop = !audio.loop;
-	} );
-}
 window.onload = function init() {
 	try {
 		// webkit shim
@@ -111,13 +73,10 @@ window.onload = function init() {
 		window.URL = window.URL || window.webkitURL;
 
 		audio_context = new AudioContext;
-		__log('Audio context set up.');
-		__log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
 	} catch (e) {
 		alert('No web audio support in this browser!');
 	}
 
 	navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
-		__log('No live audio input: ' + e);
 	});
 };
